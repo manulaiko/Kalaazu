cdb5fee7ede08f1f09173c3a8a1e536a98e88dd6
Manulaiko <manulaiko@gmail.com>
2018-04-04 10:57:36 +0200

    Create 'accounts_configurations_accounts_items'
    An account can contain many configurations, and each configuration
    can contain many items.

--- DataBase/database/accounts_configurations_accounts_items/structure.sql
+++ DataBase/database/accounts_configurations_accounts_items/structure.sql
@@ -3,9 +3,9 @@
 -- Items of the configuration.
 --
 CREATE TABLE `accounts_configurations_accounts_items` (
-    `id`                         int          NOT NULL AUTO_INCREMENT COMMENT 'Primary Key.',
-    `accounts_configurations_id` int          NULL     DEFAULT NULL,
-    `accounts_items_id`          int          NOT NULL DEFAULT 1,
+    `id`                         int NOT NULL AUTO_INCREMENT COMMENT 'Primary Key.',
+    `accounts_configurations_id` int NULL,
+    `accounts_items_id`          int NOT NULL,
 
     CONSTRAINT `accounts_configurations_accounts_items_pk` PRIMARY KEY (`id`)
 ) ENGINE InnoDB CHARACTER SET utf8 COMMENT 'Items of the configuration.';
