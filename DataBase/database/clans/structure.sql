e24dba9d0d8fbc3317c74d47a86059bc1fef84af
Manulaiko <manulaiko@gmail.com>
2018-04-06 12:02:51 +0200

    Add information columns

--- DataBase/database/clans/structure.sql
+++ DataBase/database/clans/structure.sql
@@ -5,9 +5,12 @@
 CREATE TABLE `clans` (
     `id`                 int          NOT NULL AUTO_INCREMENT COMMENT 'Primary Key.',
     `accounts_id`        int          NOT NULL                COMMENT 'Owner ID',
+    `factions_id`        int          NULL     DEFAULT NULL   COMMENT 'Clan affiliation faction.',
     `tag`                varchar(4)   NOT NULL DEFAULT ''     COMMENT 'Name abbreviation.',
     `name`               varchar(255) NOT NULL DEFAULT '',
     `description`        text         NOT NULL,
+    `logo`               varchar(255) NOT NULL DEFAULT '',
+    `status`             int          NOT NULL DEFAULT 0      COMMENT '0 = closed, 1 = recruiting, 2 = lvl10+, 3 = lvl16+, 4 = FE.',
 
     CONSTRAINT `clans_pk` PRIMARY KEY (`id`)
 ) ENGINE InnoDB CHARACTER SET utf8 COMMENT 'Server clans.';
