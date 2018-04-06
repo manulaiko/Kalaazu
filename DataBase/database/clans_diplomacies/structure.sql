0604210041997a365c5ff2cfe181b251d0451315
Manulaiko <manulaiko@gmail.com>
2018-04-06 11:09:05 +0200

    Add diplomacy expire date.

--- DataBase/database/clans_diplomacies/structure.sql
+++ DataBase/database/clans_diplomacies/structure.sql
@@ -7,6 +7,7 @@ CREATE TABLE `clans_diplomacies` (
     `from_clans_id` int       NOT NULL                           COMMENT 'Clan that made the request.',
     `to_clans_id`   int       NOT NULL                           COMMENT 'Clan that receives the request.',
     `date`          timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Diplomacy creation date.',
+    `expires`       timestamp NULL     DEFAULT NULL              COMMENT 'Date when the diplomacy expires.',
     `status`        int       NOT NULL DEFAULT 0                 COMMENT 'Status of the diplomacy. 0 = not accepted, 1 = accepted, 2 = rejected, 3 = over.',
     `type`          int       NOT NULL DEFAULT 1                 COMMENT 'Diplomacy type. 1 = War, 2 = NAP, 3 = Alliance.',
 
