c9a2e2e46cf3024b729b68937716d0863c1ac470
Manulaiko <manulaiko@gmail.com>
2018-04-06 11:52:44 +0200

    Create `accounts_destroys` table
    This table contains the ship destroy history of an account.

---
+++ DataBase/database/accounts_destroys/relations.sql
@@ -0,0 +1,6 @@
+-- Relations for the `accounts_destroys` table.
+--
+-- A destroy history belongs to an account.
+
+ALTER TABLE `accounts_destroys` ADD CONSTRAINT `accounts_destroys_accounts` FOREIGN KEY `accounts_destroys_accounts` (`accounts_id`)
+    REFERENCES `accounts` (`id`);
---
+++ DataBase/database/accounts_destroys/structure.sql
@@ -0,0 +1,15 @@
+-- Account destroys table.
+--
+-- Account's destroy history.
+--
+CREATE TABLE `accounts_destroys` (
+    `id`          int NOT NULL AUTO_INCREMENT COMMENT 'Primary Key.',
+    `accounts_id` int NOT NULL,
+    `ships_id`    int NOT NULL,
+    `points`      int NOT NULL DEFAULT 0      COMMENT 'Rank points received for destroying this ship.',
+    `amount`      int NOT NULL DEFAULT 0      COMMENT 'Times this ship has been destroyed',
+
+    CONSTRAINT `accounts_destroys_pk` PRIMARY KEY (`id`)
+) ENGINE InnoDB CHARACTER SET utf8 COMMENT 'Account''s destroy history.';
+
+CREATE UNIQUE INDEX `accounts_destroys_accounts_id_idx` ON `accounts_destroys` (`accounts_id`);
