59cd90a5bf1327b17e996cb63bc40f2bcdaed0a5
Manulaiko <manulaiko@gmail.com>
2018-04-06 11:36:52 +0200

    Create `accounts_history` table
    This table contains all the history events of an account.

---
+++ DataBase/database/accounts_history/relations.sql
@@ -0,0 +1,6 @@
+-- Relations for the `accounts_history` table.
+--
+-- A history belongs to an account.
+
+ALTER TABLE `accounts_history` ADD CONSTRAINT `accounts_history_accounts` FOREIGN KEY `accounts_history_accounts` (`accounts_id`)
+    REFERENCES `accounts` (`id`);
