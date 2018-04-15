970c71159997688edccf074fe7f3b839fa0aa67b
Manulaiko <manulaiko@gmail.com>
2018-04-15 14:12:48 +0200

    Create Event interface

---
+++ EventSystem/src/com/manulaiko/kalaazu/eventsystem/Event.java
@@ -0,0 +1,12 @@
+package com.manulaiko.kalaazu.eventsystem;
+
+/**
+ * Event interface.
+ * ================
+ *
+ * Provides a generic class to group all events.
+ *
+ * @author Manulaiko <manulaiko@gmail.com>
+ */
+public interface Event {
+}
