de780a9f84724f3a979cc8dcef437534acd5540e
Manulaiko <manulaiko@gmail.com>
2018-04-17 20:16:11 +0200

    Improve event listener
    Instead of letting the listeners define how to handle the events
    we now force the use of event handlers to reduce boilerplate.

--- EventSystem/src/com/manulaiko/kalaazu/eventsystem/EventListener.java
+++ EventSystem/src/com/manulaiko/kalaazu/eventsystem/EventListener.java
@@ -1,8 +1,12 @@
 package com.manulaiko.kalaazu.eventsystem;
 
-import lombok.NonNull;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Event listener.
@@ -12,14 +16,88 @@ import org.greenrobot.eventbus.ThreadMode;
  *
  * The method `onEvent` will be called when an event is published.
  *
+ * Usage:
+ *
+ * ```java
+ * var listener = new EventListener() {
+ *     public void initialize() {
+ *         this.addHandler(LoginEvent.class, new LoginEventHandler());
+ *     }
+ * };
+ * listener.addHandler(Event.class, new EventHandler());
+ * ```
+ *
  * @author Manulaiko <manulaiko@gmail.com>
  */
-public interface EventListener {
+public abstract class EventListener {
+    /**
+     * Events that this listener can handle.
+     */
+    private Map<Class<? extends Event>, List<Handler>> handlers = new HashMap<>();
+
+    /**
+     * Initializes the event handlers.
+     */
+    public abstract void initialize();
+
     /**
      * Listens for published events.
      *
      * @param event Published event.
      */
     @Subscribe(threadMode = ThreadMode.ASYNC)
-    void onEvent(@NonNull Object event);
+    public void onEvent(Object event) {
+        this.handlers.forEach((k, v) -> {
+            if (!k.isInstance(event)) {
+                return;
+            }
+
+            //noinspection unchecked
+            v.parallelStream()
+             .forEach(h -> h.handle(k.cast(event)));
+        });
+    }
+
+    /**
+     * Adds a new handler to the list.
+     *
+     * @param clazz   Event type that can be handled.
+     * @param handler Handler to call.
+     */
+    public void addHandler(Class<? extends Event> clazz, Handler handler) {
+        if (!clazz.isInstance(handler)) {
+            return;
+        }
+
+        var handlers = this.findHandlers(clazz);
+        handlers.add(handler);
+
+        this.handlers.put(clazz, handlers);
+    }
+
+    /**
+     * Removes a handler from the list.
+     *
+     * @param clazz   Event type that can be handled.
+     * @param handler Handler to remove.
+     */
+    public void removeHandler(Class<? extends Event> clazz, Handler<? extends Event> handler) {
+        var handlers = this.findHandlers(clazz);
+        handlers.remove(handler);
+
+        this.handlers.put(clazz, handlers);
+    }
+
+    /**
+     * Returns all handlers registered for the class.
+     *
+     * If no handler is registered, an empty list will be returned.
+     *
+     * @param clazz Event type.
+     *
+     * @return List of registered handlers.
+     */
+    private List<Handler> findHandlers(Class<? extends Event> clazz) {
+        return this.handlers.getOrDefault(clazz, new ArrayList<>());
+    }
 }
