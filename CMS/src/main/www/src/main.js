324491a0bf0eced04c5a396cb7091361b5ca327e
Manulaiko <manulaiko@gmail.com>
2018-06-26 17:30:20 +0200

    Import Eventbus client

--- CMS/src/main/www/src/main.js
+++ CMS/src/main/www/src/main.js
@@ -1,16 +1,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import { store } from './store'
+import {store} from './store'
 import Notifications from 'vue-notification'
+import VertxEventBus from 'vue-vertx3-eventbus-client'
 
-Vue.config.productionTip = false
-Vue.use(Notifications)
+Vue.config.productionTip = false;
+Vue.use(Notifications);
+Vue.use(VertxEventBus, {path: '/eventbus', port: 8080});
 
 new Vue({
   el: '#app',
   router,
   store,
-  components: { App },
+  components: {App},
   template: '<App/>'
-})
+});
