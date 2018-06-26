4c70278344d1934cfab863a069c4ac5e3b219473
Manulaiko <manulaiko@gmail.com>
2018-06-26 17:58:02 +0200

    Implement eventbus

--- CMS/src/main/www/src/components/External/Login.vue
+++ CMS/src/main/www/src/components/External/Login.vue
@@ -1,65 +1,73 @@
 <template>
-    <div id="form-login">
-        <div class="field">
-            <p class="control has-icons-left">
-                <input class="input" entity="text" placeholder="Username" v-model="username">
-                <span class="icon is-small is-left">
-                    <i class="fas fa-user"></i>
-                </span>
-            </p>
-        </div>
-        <div class="field">
-            <p class="control has-icons-left">
-                <input class="input" entity="password" placeholder="Password" v-model="password">
-                <span class="icon is-small is-left">
-                    <i class="fas fa-lock"></i>
-                </span>
-            </p>
-        </div>
-        <div class="field">
-            <input type="submit" class="button is-info is-inverted is-outlined" @click.prevent="execute()" v-model="submit">
-        </div>
+  <div id="form-login">
+    <div class="field">
+      <p class="control has-icons-left">
+        <input class="input" type="text" placeholder="Username" v-model="username">
+        <span class="icon is-small is-left">
+          <i class="fas fa-user"></i>
+        </span>
+      </p>
     </div>
+    <div class="field">
+      <p class="control has-icons-left">
+        <input class="input" type="password" placeholder="Password" v-model="password">
+        <span class="icon is-small is-left">
+          <i class="fas fa-lock"></i>
+        </span>
+      </p>
+    </div>
+    <div class="field">
+      <input type="submit" class="button is-info is-inverted is-outlined" @click.prevent="execute()" v-model="submit">
+    </div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
+  export default {
+    data() {
+      return {
+        submit: 'Login',
 
-export default {
-  data() {
-    return {
-      submit: 'Login',
+        username: '',
+        password: ''
+      }
+    },
+    methods: {
+      /**
+       * Submits the form.
+       */
+      execute: function () {
+        this.$set(this.$data, 'submit', 'Logging in...');
 
-      username: '',
-      password: ''
-    }
-  },
-  methods: {
-    /**
-     * Submits the form.
-     */
-    execute: function() {
-      this.$set(this.$data, 'submit', 'Logging in...')
-
-      axios.get('http://localhost/External/login.php', {
-        params: {
+        const data = {
           username: this.username,
           password: this.password
-        }
-      }).then(r => {
-        if(r.data.isError) {
-          this.$notify({
-            type: 'error',
-            text: r.data.message
-          })
-        } else {
-          this.$store.state.sessionId = r.data.sessionId
-          this.$router.push('/Internal/Start')
-        }
+        };
+        this.$eventBus.send('persistence.login', data, (err, res) => {
+          this.$set(this.$data, 'submit', 'Login');
 
-        this.$set(this.$data, 'submit', 'Login')
-      });
+          if (err != null) {
+            this.$notify({
+              type: 'error',
+              text: err.failureType + ' (' + err.failureCode + '): ' + err.message
+            });
+
+            return;
+          }
+
+          if (res.body.isError) {
+            this.$notify({
+              type: 'error',
+              text: res.body.message
+            });
+
+            return;
+          }
+
+          this.$store.state.sessionId = res.body.sessionId;
+          this.$router.push('/Internal/Start');
+        });
+      }
     }
   }
-}
 </script>
--- CMS/src/main/www/src/components/External/Register.vue
+++ CMS/src/main/www/src/components/External/Register.vue
@@ -1,97 +1,103 @@
 <template>
-    <div id="form-register">
-        <div class="field">
-            <p class="control has-icons-left">
-                <input class="input" entity="text" placeholder="Username" v-model="username">
-                <span class="icon is-small is-left">
-                    <i class="fas fa-user"></i>
-                </span>
-            </p>
-        </div>
-        <div class="field">
-            <p class="control has-icons-left">
-                <input class="input" entity="password" placeholder="Password" v-model="password">
-                <span class="icon is-small is-left">
-                    <i class="fas fa-lock"></i>
-                </span>
-            </p>
-        </div>
-        <div class="field">
-            <p class="control has-icons-left">
-                <input class="input" entity="email" placeholder="Email" v-model="email">
-                <span class="icon is-small is-left">
-                    <i class="fas fa-envelope"></i>
-                </span>
-            </p>
-        </div>
-        <div class="field">
-            <p class="control has-icons-left">
-                <input class="input" entity="text" placeholder="Invitation code" v-model="code">
-                <span class="icon is-small is-left">
-                    <i class="fas fa-barcode"></i>
-                </span>
-            </p>
-        </div>
-        <div class="field control">
-            <label class="label">Faction</label>
-            <div class="select is-fullwidth">
-                <select v-model="faction">
-                    <option value="1" selected>MMO</option>
-                    <option value="2">EIC</option>
-                    <option value="3">VRU</option>
-                </select>
-            </div>
-        </div>
-        <div class="field">
-            <input type="submit" class="button is-info is-inverted is-outlined" @click.prevent="execute()" v-model="submit">
-        </div>
+  <div id="form-register">
+    <div class="field">
+      <p class="control has-icons-left">
+        <input class="input" type="text" placeholder="Username" v-model="username">
+        <span class="icon is-small is-left">
+          <i class="fas fa-user"></i>
+        </span>
+      </p>
     </div>
+    <div class="field">
+      <p class="control has-icons-left">
+        <input class="input" type="password" placeholder="Password" v-model="password">
+        <span class="icon is-small is-left">
+          <i class="fas fa-lock"></i>
+        </span>
+      </p>
+    </div>
+    <div class="field">
+      <p class="control has-icons-left">
+        <input class="input" type="email" placeholder="Email" v-model="email">
+        <span class="icon is-small is-left">
+          <i class="fas fa-envelope"></i>
+        </span>
+      </p>
+    </div>
+    <div class="field">
+      <p class="control has-icons-left">
+        <input class="input" type="text" placeholder="Invitation code" v-model="code">
+        <span class="icon is-small is-left">
+          <i class="fas fa-barcode"></i>
+        </span>
+      </p>
+    </div>
+    <div class="field control">
+      <label class="label" for="faction">Faction</label>
+      <div class="select is-fullwidth">
+        <select v-model="faction" id="faction">
+          <option value="1" selected>MMO</option>
+          <option value="2">EIC</option>
+          <option value="3">VRU</option>
+        </select>
+      </div>
+    </div>
+    <div class="field">
+      <input type="submit" class="button is-info is-inverted is-outlined" @click.prevent="execute()" v-model="submit">
+    </div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
+  export default {
+    data() {
+      return {
+        submit: 'Register',
 
-export default {
-  data() {
-    return {
-      submit: 'Register',
+        username: '',
+        password: '',
+        email: '',
+        code: '',
+        faction: '1'
+      }
+    },
+    methods: {
+      /**
+       * Submits the form.
+       */
+      execute: function () {
+        this.$set(this.$data, 'submit', 'Registering...');
 
-      username: '',
-      password: '',
-      email: '',
-      code: '',
-      faction: '1'
-    }
-  },
-  methods: {
-    /**
-     * Submits the form.
-     */
-    execute: function() {
-      this.$set(this.$data, 'submit', 'Registering...')
-
-      axios.get('http://localhost/External/register.php', {
-        params: {
+        const data = {
           username: this.username,
           password: this.password,
           email: this.email,
           code: this.code,
           faction: this.faction
-        }
-      }).then(r => {
-        if(r.data.isError) {
-          this.$notify({
-            type: 'error',
-            text: r.data.message
-          })
-        } else {
-          this.$store.state.sessionId = r.data.sessionId
-          this.$router.push('/Internal/Start')
-        }
-      });
+        };
+        this.$eventBus.send('persistence.register', data, (err, res) => {
+          this.$set(this.$data, 'submit', 'Register');
 
-      this.$set(this.$data, 'submit', 'Register')
+          if (err != null) {
+            this.$notify({
+              type: 'error',
+              text: err.failureType + ' (' + err.failureCode + '): ' + err.message
+            });
+
+            return;
+          }
+
+          if (res.body.isError) {
+            this.$notify({
+              type: 'error',
+              text: res.body.message
+            });
+          }
+
+          this.$store.state.sessionId = res.body.sessionId;
+          this.$router.push('/Internal/Start');
+        });
+      }
     }
   }
-}
 </script>
