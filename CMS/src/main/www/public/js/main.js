75ed81d080ff414bf277d32c17b06e0ef0b8099d
manulaiko <manulaiko@gmail.com>
2019-10-31 16:32:47 +0100

    Recreate CMS frontend

--- CMS/src/main/www/public/js/main.js
+++
@@ -1,7 +0,0 @@
-String.prototype.wrap = function (length, ellipses = '...') {
-  if (this.length < length + ellipses.length) {
-    return this.toString();
-  }
-
-  return this.substr(0, length) + ellipses;
-};
