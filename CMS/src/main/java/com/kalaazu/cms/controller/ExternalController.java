b5a986f92c78dadbc9460b9feac786379c2af2f9
manulaiko <manulaiko@gmail.com>
2019-11-04 17:45:49 +0100

    Create login endpoint logic

--- CMS/src/main/java/com/kalaazu/cms/controller/ExternalController.java
+++ CMS/src/main/java/com/kalaazu/cms/controller/ExternalController.java
@@ -3,12 +3,19 @@ package com.kalaazu.cms.controller;
 import com.kalaazu.cms.dto.LoginRequest;
 import com.kalaazu.cms.dto.LoginResponse;
 import com.kalaazu.cms.dto.Response;
+import com.kalaazu.persistence.service.AccountsService;
+import com.kalaazu.persistence.service.UsersService;
+import com.kalaazu.util.StringUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.util.Date;
 
 /**
  * External controller.
@@ -22,6 +29,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/external")
 public class ExternalController {
+    @Autowired
+    private UsersService usersService;
+
+    @Autowired
+    private AccountsService accountsService;
+
     /**
      * Login endpoint.
      *
@@ -34,11 +47,48 @@ public class ExternalController {
         var response = new Response<LoginResponse>();
         response.setKind("unknown");
 
-        log.info(body.toString());
-
         if (body.getUsername().isEmpty() || body.getPassword().isEmpty()) {
             response.setKind("bad-data");
+
+            return ResponseEntity.ok(response);
         }
+
+        var user = usersService.login(body.getUsername(), body.getPassword());
+        if (user == null) {
+            response.setKind("not-found");
+
+            return ResponseEntity.ok(response);
+        }
+        // Filter out banned accounts.
+        // Last logged in account.
+        var lastUsedAccount = user.getAccounts()
+                                  .stream()
+                                  .filter(
+                                          // Filter out banned accounts.
+                                          a -> a.getBanDate() != null &&
+                                               a.getBanDate().compareTo(Date.from(Instant.now())) < 0
+                                  )
+                                  .min(
+                                          // Last logged in account.
+                                          (a1, a2) -> a2.getLastLogin().compareTo(a1.getLastLogin())
+                                  )
+                                  .orElse(null);
+
+        if (lastUsedAccount == null) {
+            response.setKind("rejected");
+
+            return ResponseEntity.ok(response);
+        }
+
+        // Generate new session ID.
+        lastUsedAccount.setSessionId(StringUtils.random(32));
+        this.accountsService.update(lastUsedAccount);
+
+        var data = new LoginResponse();
+        data.setSessionId(lastUsedAccount.getSessionId());
+
+        response.setKind("ok");
+        response.setData(data);
 
         return ResponseEntity.ok(response);
     }
