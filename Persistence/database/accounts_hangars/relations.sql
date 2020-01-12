6e436e73825f97cdee9272114cc0e7905edfd894
manulaiko <manulaiko@gmail.com>
2020-01-12 17:37:52 +0100

    Add hangar - configuration relationship

--- Persistence/database/accounts_hangars/relations.sql
+++ Persistence/database/accounts_hangars/relations.sql
@@ -2,6 +2,7 @@
 --
 -- An hangar belongs to an account.
 -- An hangar has a ship.
+-- An hangar has an active configuration.
 
 ALTER TABLE `accounts_hangars`
     ADD CONSTRAINT `accounts_hangars_accounts` FOREIGN KEY `accounts_hangars_accounts` (`accounts_id`)
@@ -10,3 +11,7 @@ ALTER TABLE `accounts_hangars`
 ALTER TABLE `accounts_hangars`
     ADD CONSTRAINT `accounts_hangars_accounts_ships` FOREIGN KEY `accounts_hangars_accounts_ships` (`accounts_ships_id`)
         REFERENCES `accounts_ships` (`id`);
+
+ALTER TABLE `accounts_hangars`
+    ADD CONSTRAINT `accounts_hangars_accounts_configurations` FOREIGN KEY `accounts_hangars_accounts_configurations` (`accounts_configurations_id`)
+        REFERENCES `accounts_configurations` (`id`);
--- Persistence/database/dump.sql
+++ Persistence/database/dump.sql
@@ -8226,6 +8226,7 @@ ALTER TABLE `accounts_galaxygates`
 --
 -- An hangar belongs to an account.
 -- An hangar has a ship.
+-- An hangar has an active configuration.
 
 ALTER TABLE `accounts_hangars`
     ADD CONSTRAINT `accounts_hangars_accounts` FOREIGN KEY `accounts_hangars_accounts` (`accounts_id`)
@@ -8234,6 +8235,10 @@ ALTER TABLE `accounts_hangars`
 ALTER TABLE `accounts_hangars`
     ADD CONSTRAINT `accounts_hangars_accounts_ships` FOREIGN KEY `accounts_hangars_accounts_ships` (`accounts_ships_id`)
         REFERENCES `accounts_ships` (`id`);
+
+ALTER TABLE `accounts_hangars`
+    ADD CONSTRAINT `accounts_hangars_accounts_configurations` FOREIGN KEY `accounts_hangars_accounts_configurations` (`accounts_configurations_id`)
+        REFERENCES `accounts_configurations` (`id`);
 
 -- Relations for the `accounts_history` table.
 --
